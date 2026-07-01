09bf70ca405c61396b1b3c2d0b876480a9406bec
nuel110706 <nuelcorputty@gmail.com>
2026-07-01 12:48:05 +0700

    Prepare Flask app for Railway deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import os
 from app import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(__import__('os').getenv('PORT', '8080')))
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '8080')), debug=False)
